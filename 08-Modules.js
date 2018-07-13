f7526e8dd789b8d5a56df2f21e95f68c9eb3541b
Felipe Ospina <pipe.98.oh@gmail.com>
2018-07-13 14:52:46 -0500

    How to import modules without clases (ES6).

---
+++ 08-Modules.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const My_Data = require('./my-data.js')
+
+console.log(My_Data.name, My_Data.email, My_Data)
---
+++ my-data.js
@@ -0,0 +1,8 @@
+'use strict'
+
+let name = 'Felipe',
+    email = 'pipe-oh@hotmail.com',
+    _phone = '3012109092' //To make it privatly
+
+module.exports.name = name
+module.exports.email = email
