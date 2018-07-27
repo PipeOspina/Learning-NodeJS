5fba2734339def391712014190ea432ab718cfe8
Felipe Ospina <pipe.98.oh@gmail.com>
2018-07-26 22:35:46 -0500

    NPM Modules/Globals created and setup for debbuging.

--- 02ModulosNPM/00Globales/01-Debugger.js
+++ 02ModulosNPM/00Globales/01-Debugger.js
@@ -1,20 +1,10 @@
 'use strict'
 
-/*
-const http = require('http')
-
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type':'text/plain'})
-    res.end('Hello World!')
-}).listen(3000, 'localhost')
-
-console.log('Server running at http://localhost:3000')
-*/
-
 const http = require('http')
 
 function webServer(req, res) {
     res.writeHead(200, {'Content-Type':'text/html'})
+    debugger;
     res.end(
         '<html>'
         +   '<head>'
