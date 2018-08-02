d2e1c1e29bbef6093b5f38b7faf941d10c1407e6
Felipe Ospina <pipe.98.oh@gmail.com>
2018-08-01 20:20:47 -0500

    Finished callback.js.

--- 03Promesas/callback.js
+++ 03Promesas/callback.js
@@ -2,6 +2,22 @@
 
 const fs = require('fs');
 
-fs.exists("assets/names.txt", (exists) => {
-  if(exists) console.log("El archivo existe"); else console.log("El archivo no existe");
+let path = './assets/';
+let fileName = 'names.txt';
+let newFileName = 'names_callback.txt';
+
+fs.access(path + fileName, fs.constants.F_OK, (err) => {
+  if(err) console.log('El archivo no existe');
+  else {
+    console.log('El archivo existe');
+    fs.readFile(path + fileName, (err, data) => {
+      if(err) console.log('El archivo no se pudo leer');
+      else {
+        console.log('El archivo se ha leido exitosamente');
+        fs.writeFile(path + newFileName, data, (err) => {
+          return (err) ? console.log('El archivo no se pudo copiar') : console.log('El archivo se ha copiado con exito');
+        });
+      }
+    });
+  }
 });
