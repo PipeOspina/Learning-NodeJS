4a5db0fb3269da141fc15ba5bd10480ce3826f78
Felipe Ospina <pipe.98.oh@gmail.com>
2018-08-03 09:24:38 -0500

    Working at response method, .render() function.

--- 04Express/00Basico/03-Hello-Express-response.js
+++ 04Express/00Basico/03-Hello-Express-response.js
@@ -7,8 +7,31 @@ function index(req, res) {
   res.sendFile(`${__dirname}/assets/index.html`);
 }
 
+function bextlan(req, res) {
+  //res.send('<h1>Bienvenid@s a Bextl&aacute;n... Lugar de bits, vectores y pixeles.</h1>');
+  res.redirect(301, 'https://jonmircha.com');
+}
+
+function json(req, res) {
+  res.json({
+    name: "Felipe",
+    age: 20,
+    twitter: "@PipeOxpina"
+  });
+}
+
+function render(req, res) {
+  //Se soluciona en la siguiente clase :)
+  res.render(`${__dirname}/assets/index.html`, (err, html) => {
+    res.send(html);
+  });
+}
+
 app
   .get('/', index)
+  .get('/bextlan', bextlan)
+  .get('/json', json)
+  .get('/render', render)
   .listen(port);
 
 console.log(`Running Express app at http://localhost:${port}`)
