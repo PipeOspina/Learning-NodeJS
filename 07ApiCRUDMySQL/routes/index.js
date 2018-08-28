f0bb27c51b99479cb420e456bb5e7dc407445a10
Felipe Ospina <pipe.98.oh@gmail.com>
2018-08-28 11:36:57 -0500

    Setting up the view of page
    Establishing connection and index configuration for DB and index view.

--- 07ApiCRUDMySQL/routes/index.js
+++ 07ApiCRUDMySQL/routes/index.js
@@ -1,40 +1,19 @@
 'use strict'
 
 const express = require('express');
+const movies = require('../models/movies');
 const router = express.Router();
 
-router
-  .get('/', index)
-  .get('/pug', pug)
-  .get('/ejs', ejs)
-  .use(error404);
-
 function index(req, res) {
-  res.end(`<h1>Terminamos la configuraci&oacute;n de nuestra primer App en Express</h1>`);
-}
-
-function pug(req, res) {
-  let locals = {
-    title: 'Pug =D',
-    link: 'https://pugjs.org/api/getting-started.html',
-    description: 'Pug 2 was released in August 2016. To make improvements in the new release possible, we had to make the decision of deprecating or removing some APIs and undocumented language features. We made an effort to make these changes as unintrusive as possible, and many of these changes were previously discouraged in console warnings.'
-  };
-  res.render('index', locals);
-}
-
-function ejs(req, res) {
-  let locals = {
-    title: 'EJS =D',
-    link: 'http://www.embeddedjs.com/',
-    description: 'EJS limpia el HTML del JavaScript con plantillas del lado cliente. Combina datos y una plantilla para producir HTML. Codigo entre <%= %> lo añade al HTML que se resuelve.',
-    seasons: [
-      ['Primavera', ['Abril', 'Mayo', 'Junio']],
-      ['Verano', ['Julio', 'Agosto', 'Septiembre']],
-      ['Otoño', ['Octubre', 'Noviembre', 'Diciembre']],
-      ['Invierno', ['Enero', 'Febrero', 'Marzo']]
-    ]
-  };
-  res.render('index', locals);
+  req.getConnection((err, movies) => {
+    movies.query('SELECT * FROM movie', (err, rows) => {
+      let locals = {
+        title: 'Lista de Películas',
+        data: rows
+      }
+      res.render('index', locals);
+    });
+  });
 }
 
 function error404(req, res, next) {
@@ -50,5 +29,10 @@ function error404(req, res, next) {
 
   next();
 }
+
+router
+  .use(movies)
+  .get('/', index)
+  .use(error404);
 
 module.exports = router;
